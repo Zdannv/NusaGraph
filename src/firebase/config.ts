142aaf7c9709fd64b3d5b5b37b6e5e06f761d288
Muhammad Zaidan Zhafiz Satrianto <zaidanzhafifsatrianto@gmail.com>
2026-02-24 06:19:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2187071910-dd74d",
+  "appId": "1:993766639124:web:567450311b9d4679318901",
+  "apiKey": "AIzaSyCSdNQl2UNgPF5jtKyhtosam6cIkHeJ26E",
+  "authDomain": "studio-2187071910-dd74d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "993766639124"
+};
